46712c3ec63df051e2f274c2f530f9ac7e8a1d5b
pho <pho@damnedsouls.de>
2025-01-01 03:17:02 +0100

    feat: add regex patterns for TV show season and episode extraction
    - Introduced a new RegexTv class to handle regular expressions for parsing TV show titles, seasons, and episodes.
    - Added two regex patterns: BasicSeasonEpisodeRegexPattern for extracting season and episode information, and FinerTitleRegexPattern for capturing titles with optional year.
    - Implemented generated regex methods for optimized performance and case-insensitive matching.

---
+++ src/PlexLocalScan.Shared/Services/RegexTv.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PlexLocalScan.Shared.Services;
+
+public abstract partial class RegexTv
+{
+    [GeneratedRegex(@"^(?<title>.+?)[\. \[]?[Ss](?<season>\d{1,2})[\. \[]?[eE](?<episode>\d{1,2})?[-]?(?:[-eE](?<episode2>\d{1,2}))?.*\.(mkv|mp4|avi)$", RegexOptions.IgnoreCase | RegexOptions.Compiled, "en-EN")]
+    public static partial Regex MyRegex();
+
+    [GeneratedRegex(@"^(?<title>.+?)(?:\s\(?(?<year>\d{4})\)?)?\s?[-\s]*$", RegexOptions.IgnoreCase | RegexOptions.Compiled, "en-EN")]
+    public static partial Regex MyRegex1();
+}
